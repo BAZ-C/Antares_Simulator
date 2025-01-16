80ce4ffe56c83b13069bd364b064c7b9f6f6a96f
Abdoulbari Zakir <abdoulbari.zakir@gmail.com>
2025-01-16 11:16:27 +0100

    dd

--- src/solver/modeler/ortoolsImpl/linearProblem.cpp
+++ src/solver/modeler/ortoolsImpl/linearProblem.cpp
@@ -31,7 +31,7 @@
 namespace Antares::Solver::Modeler::OrtoolsImpl
 {
 
-OrtoolsLinearProblem::OrtoolsLinearProblem(bool isMip, const std::string& solverName)
+OrtoolsLinearProblem::OrtoolsLinearProblem            (bool isMip, const std::string& solverName)
 {
     mpSolver_ = MPSolverFactory(isMip, solverName);
     objective_ = mpSolver_->MutableObjective();
@@ -40,13 +40,17 @@ OrtoolsLinearProblem::OrtoolsLinearProblem(bool isMip, const std::string& solver
 class ElemAlreadyExists: public std::exception
 {
 public:
-    const char* what() const noexcept override
+    const char* what()               const noexcept override
     {
         return "Element name already exists in linear problem";
     }
 };
 
 OrtoolsMipVariable* OrtoolsLinearProblem::addVariable(double lb,
+
+
+
+
                                                       double ub,
                                                       bool integer,
                                                       const std::string& name)
